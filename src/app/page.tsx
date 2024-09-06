09a4c0b51fab2fc3a2692a27cfaead80d1fa3729
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-06 09:34:55 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import {add, differenceInSeconds, endOfWeek, intervalToDuration, startOfWeek, Duration} from "date-fns";
+import no from "date-fns/locale/nb";
+import ProgressBar from "@/components/ProgressBar";
+import React from "react";
+
+export default function Home() {
+
+    const now: Date = new Date();
+    const start: Date = startOfWeek(now, { locale: no });
+    const mondayStartOfWorkday: Date = add(start, { hours: 8 });
+    const fridayEndOfWorkDay: Date = add(start, { days: 4, hours: 16 });
+    const diffBetweenMondayWorkdayAndEow: number = differenceInSeconds(fridayEndOfWorkDay, mondayStartOfWorkday);
+    const diffFromTuesday: number = differenceInSeconds(now, mondayStartOfWorkday);
+    const percentToEndOfWorkDayFriday = clamp((diffFromTuesday / diffBetweenMondayWorkdayAndEow) * 100)
+
+    return (
+        <main>
+            <div className="flex flex-col items-center justify-between p-10">
+            </div>
+            <div className="flex min-h-screen flex-col items-center justify-between md:p-12">
+                <ProgressBar progress={percentToEndOfWorkDayFriday} progressText={"Hvor mange prosent er det til helg?"} />
+            </div>
+        </main>
+    )
+}
+
+function clamp(value: number) {
+    return Math.max(Math.min(value, 100), 0);
+}
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+export type ProgressBarProps = {
+    progress: number;
+    progressText?: string;
+};
+
+const ProgressBar = ({progress, progressText}: ProgressBarProps) => {
+    return (
+        <div className="flex flex-col items-center justify-center w-1/2 lg:w-1/3">
+            <h1 className="lg:text-3xl">{progressText}</h1>
+            <div className="mt-6 w-full border-2 border-indigo-700 h-6 rounded-md">
+                <div
+                    className="bg-indigo-500 h-full transition-all duration-250"
+                    style={{width: `${progress}%`}}
+                ></div>
+            </div>
+            <p>{progress.toFixed(3)}%</p>
+        </div>
+    );
+};
+
+export default ProgressBar;
