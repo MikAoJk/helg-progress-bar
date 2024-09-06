a21599f3e30b3a212bdadf440e3af0eb22bcf1ef
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-06 13:36:22 +0200

    chore: use state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
+'use client';
+
 import {add, differenceInSeconds, startOfWeek} from "date-fns";
 import {nb} from "date-fns/locale/nb";
 import ProgressBar from "@/components/ProgressBar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Home() {
 
     const now: Date = new Date();
-    const start: Date = startOfWeek(now, { locale: nb });
-    const mondayStartOfWorkday: Date = add(start, { hours: 6 }); //due to UTC +2
-    const fridayEndOfWorkDay: Date = add(start, { days: 4, hours: 14 }); //due to UTC +2
+    const start: Date = startOfWeek(now, {locale: nb});
+    const mondayStartOfWorkday: Date = add(start, {hours: 6}); //due to UTC +2
+    const fridayEndOfWorkDay: Date = add(start, {days: 4, hours: 14}); //due to UTC +2
     const diffBetweenMondayWorkdayAndEow: number = differenceInSeconds(fridayEndOfWorkDay, mondayStartOfWorkday);
     const diffFromTuesday: number = differenceInSeconds(now, mondayStartOfWorkday);
-    const percentToEndOfWorkDayFriday = clamp((diffFromTuesday / diffBetweenMondayWorkdayAndEow) * 100)
+
+    const [percentToEndOfWorkDayFriday, setpercentToEndOfWorkDayFriday] = useState<number>(clamp((diffFromTuesday / diffBetweenMondayWorkdayAndEow) * 100));
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('Logs every minute');
+            setpercentToEndOfWorkDayFriday(clamp((diffFromTuesday / diffBetweenMondayWorkdayAndEow) * 100))
+        }, 60000);
+        return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    }, []);
 
     return (
         <main>
             <div className="flex flex-col items-center justify-between p-10">
             </div>
             <div className="flex min-h-screen flex-col items-center justify-between md:p-12">
-                <ProgressBar progress={percentToEndOfWorkDayFriday} progressText={"Hvor mange prosent er det til helg?"} />
+                <ProgressBar progress={percentToEndOfWorkDayFriday}
+                             progressText={"Hvor mange prosent er det til helg?"}/>
             </div>
         </main>
     )
