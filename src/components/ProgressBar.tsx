4b81f431e0bcb6ff966f48663b44102e0ec5bf5a
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-06 13:46:45 +0200

    chore: change text size

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -8,7 +8,7 @@ export type ProgressBarProps = {
 const ProgressBar = ({progress, progressText}: ProgressBarProps) => {
     return (
         <div className="flex flex-col items-center justify-center w-1/2 lg:w-1/3">
-            <h1 className="lg:text-3xl">{progressText}</h1>
+            <h1 className="text-xl lg:text-3xl">{progressText}</h1>
             <div className="mt-6 w-full border-2 border-indigo-700 h-6 rounded-md">
                 <div
                     className="bg-indigo-500 h-full transition-all duration-250"
